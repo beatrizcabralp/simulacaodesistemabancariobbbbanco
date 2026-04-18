ac193c0b1d308cc64fd87ada438e0146d7c690c6
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2026-04-17 21:52:50 -0300

    Adicionando o menu

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, seja muito bem vindo ao BBB - BibisBanco!!!");
+        System.out.println("Por favor, digite o número de qual funcionalidade deseja acessar:\n");
+        System.out.println("\n******************************\n           Menu\n******************************\n\n");
+        System.out.println("1 - Criar Conta;");
+        System.out.println("2 - Realizar Depósito;");
+        System.out.println("3 - Realizar Saque;");
+        System.out.println("4 - Realizar transferências;");
+        System.out.println("5 - Listar Contas;");
+        System.out.println("6 - Calcular Total de Tributos;");
+        System.out.println("7 - Sair;");
+
+        int numeroDigitado = scanner.nextInt();
+
+        while(numeroDigitado < 1 || numeroDigitado > 7){
+            System.out.println("Número inválido! Por favor, digite um número válido:");
+
+            System.out.println("\n******************************\n           Menu\n******************************\n\n");
+            System.out.println("1 - Criar Conta;");
+            System.out.println("2 - Realizar Depósito;");
+            System.out.println("3 - Realizar Saque;");
+            System.out.println("4 - Realizar transferências;");
+            System.out.println("5 - Listar Contas;");
+            System.out.println("6 - Calcular Total de Tributos;");
+            System.out.println("7 - Sair;");
+
+            numeroDigitado = scanner.nextInt();
+        }
+
+        System.out.println("O número digitado foi " + numeroDigitado);
     }
 }
