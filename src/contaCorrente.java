c26e6294edfc1d0c2de48991bacc995d163d9e17
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2026-04-22 20:50:52 -0300

    Acrscentando as classes conta poupança e conta corrente

--- src/contaCorrente.java
+++ src/contaCorrente.java
@@ -1,2 +1,31 @@
-public class contaCorrente {
+public class contaCorrente extends criarConta {
+
+    public contaCorrente(int numero, String cliente) {
+        super(numero, cliente);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        double taxa = valor * 0.05;
+        double total = valor + taxa;
+
+        if (saldo >= total) {
+            saldo -= total;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    @Override
+    public void transferir(criarConta destino, double valor) {
+        double taxa = valor * 0.05;
+        double total = valor + taxa;
+
+        if (saldo >= total) {
+            saldo -= total;
+            destino.depositar(valor);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
 }
--- src/contaPoupanca.java
+++ src/contaPoupanca.java
@@ -1,2 +1,25 @@
-public class contaPoupanca {
+public class contaPoupanca extends criarConta {
+
+    public contaPoupanca(int numero, String cliente) {
+        super(numero, cliente);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+
+    @Override
+    public void transferir(criarConta destino, double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            destino.depositar(valor);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
 }
